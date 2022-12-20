61d74a9337028609cc0be77d329c6b19a36b282e
Andy Rudnitsky <andy.rudnitsky@gmail.com>
2022-12-19 23:21:54 -0500

    Updates: standalone script, use stdin or file

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # stress-csv
-Quick python script to add pronunciation stress marks to csv files intended for importing into Anki. The script expects the first field to be in ukrainian.
+
+Quick and dirty python script to add pronunciation stress marks to csv files intended for importing into Anki.
+- The script expects the first field to be in Ukrainian.
+- Input can be read from a file, or provided on stdin.
+- The separator is assumed to be a comma.
+- Fields with commas must be quoted.
--- stress-csv.py
+++ stress-csv.py
@@ -1,12 +1,24 @@
+#!/opt/homebrew/bin/python3
 import sys
 import csv
 from ukrainian_word_stress import Stressifier, StressSymbol
 
-
 if __name__ == "__main__":
     stressify = Stressifier(stress_symbol=StressSymbol.CombiningAcuteAccent)
-    with open(sys.argv[1], newline="") as csvfile:
-        csvreader = list(csv.reader(csvfile, delimiter=","))
-        for row in csvreader:
-            if ( row[0][0] != "#"):
-                print(stressify(row[0]) + "," + ",".join(row[1:]))
+
+    if len(sys.argv) != 1:
+        inf = open(sys.argv[1])
+    else:
+        inf = sys.stdin
+
+    csvreader = list(csv.reader(inf, delimiter=","))
+    for row in csvreader:
+        if row[0][0] != "#":
+            print(stressify(row[0]), end="")
+            fieldCount = len(row) - 1
+            for index in range(0, len(row) - 1):
+                print("," + row[index + 1], end="")
+            print()
+
+    if inf is not sys.stdin:
+        inf.close()
